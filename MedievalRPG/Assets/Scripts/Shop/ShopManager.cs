26319f111de8241c32f8ea3700a241e949a0b336
Enteryte <58777447+Enteryte@users.noreply.github.com>
2022-10-05 16:20:47 +0200

    ShopManager, Interacting
    Scripts hinzugefügt: ShopManager + Interacting

---
+++ MedievalRPG/Assets/Scripts/Shop/ShopManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopManager : MonoBehaviour
+{
+    public static MerchantBaseProfile currMBP;
+    public static ShopListBaseProfile currSLBP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
