b128fb310880872c7905b3a783d2c842914d9cf8
Enteryte <58777447+Enteryte@users.noreply.github.com>
2022-10-24 00:49:08 +0200

    Prick (Minigame)

---
+++ MedievalRPG/Assets/Scripts/Minigames/Prick/PrickBoard.cs
@@ -0,0 +1,58 @@
+using StarterAssets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrickBoard : MonoBehaviour, IInteractable
+{
+    [HideInInspector] public InteractableObjectCanvas iOCanvas;
+
+    public Transform prickManagerTrans;
+
+    void Start()
+    {
+        InstantiateIOCanvas();
+    }
+
+    public void InstantiateIOCanvas()
+    {
+        GameObject newIOCanvas = Instantiate(Interacting.instance.interactCanvasPrefab, Interacting.instance.iOCSParentObj.transform);
+
+        newIOCanvas.GetComponent<InteractableObjectCanvas>().correspondingGO = this.gameObject;
+
+        iOCanvas = newIOCanvas.GetComponent<InteractableObjectCanvas>();
+
+        newIOCanvas.transform.SetAsFirstSibling();
+    }
+
+    public void Interact(Transform transform)
+    {
+        prickManagerTrans.GetComponent<PrickMinigameManager>().enabled = true;
+        //gTCManagerTrans.GetComponent<GuessTheCardMinigameManager>().gTCUI.SetActive(true);
+        //gTCManagerTrans.GetComponent<GuessTheCardMinigameManager>().gTCCamera.enabled = true;
+
+        GameManager.instance.FreezeCameraAndSetMouseVisibility(ThirdPersonController.instance, ThirdPersonController.instance._input, false);
+
+        ThirdPersonController.instance._animator.SetFloat("Speed", 0);
+
+        for (int i = 0; i < MessageManager.instance.collectedMessageParentObj.transform.childCount; i++)
+        {
+            Destroy(MessageManager.instance.collectedMessageParentObj.transform.GetChild(i).gameObject);
+        }
+    }
+
+    public string GetInteractUIText()
+    {
+        return "Spielen";
+    }
+
+    public float GetTimeTillInteract()
+    {
+        return 1.5f;
+    }
+
+    InteractableObjectCanvas IInteractable.iOCanvas()
+    {
+        return this.iOCanvas;
+    }
+}
