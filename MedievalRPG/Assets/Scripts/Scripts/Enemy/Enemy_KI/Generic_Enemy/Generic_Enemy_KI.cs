b6b14379d2ef55a90c8b9959a178c65047f72ea5
Juri Weber <games@jomshanse.de>
2022-11-24 20:39:27 +0100

    Added basic AI-Related things
    Added a Scene for AI Testing,
    an AI Stats SO for AI Control
    The beginning of AI Code
    Gave the Skeleton an emergency solution for Damage

---
+++ MedievalRPG/Assets/Scripts/Scripts/Enemy/Enemy_KI/Generic_Enemy/Generic_Enemy_KI.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generic_Enemy_KI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void NoticeEnemy()
+    {
+        
+    }
+
+    private void CheckAttackPossible()
+    {
+        
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        
+    }
+}
