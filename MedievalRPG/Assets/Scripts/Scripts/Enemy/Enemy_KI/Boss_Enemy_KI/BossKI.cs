828139153cb21508f79b18c6223db732ec0ace4a
Juri Weber <43060805+Jomshanseat@users.noreply.github.com>
2023-01-09 17:44:29 +0100

    Started the boss attacks
    Started the scripts and integrated some new and updated attacks.

---
+++ MedievalRPG/Assets/Scripts/Scripts/Enemy/Enemy_KI/Boss_Enemy_KI/BossKI.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossKI : BaseEnemyKI
+{
+    public override void Init()
+    {
+        base.Init();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    public override void Death()
+    {
+        base.Death();
+    }
+
+    protected override void OnDrawGizmos()
+    {
+        base.OnDrawGizmos();
+    }
+}
---
+++ MedievalRPG/Assets/Scripts/Scripts/Enemy/Enemy_KI/Boss_Enemy_KI/EnemyAttackController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttackController : MonoBehaviour
+{
+
+    public void FireShockWave()
+    {
+        
+    }
+
+    public void DropMeteor()
+    {
+        
+    }
+
+    public void SideCleaning()
+    {
+        
+    }
+
+    public void TargetedFire()
+    {
+        
+    }
+}
