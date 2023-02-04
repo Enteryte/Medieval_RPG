e2d6e900a07a11ea2ee10c53e58a545a1235cbb0
Juri Weber <43060805+Jomshanseat@users.noreply.github.com>
2023-02-04 21:52:43 +0100

    Gave Zombies their Animations and fixed the Damagers
    Damagers have some slight connectivity problems due to GotHit not being on the Colliding Part of the player.
    Player to Enemy Damage also needs some slight tweaking to work. Probs also should do the No-Shield Melee Skeleton

--- MedievalRPG/Assets/Scripts/Scripts/Enemy/EnemyDamager.cs
+++ MedievalRPG/Assets/Scripts/Scripts/Enemy/EnemyDamager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -6,8 +7,14 @@ public class EnemyDamager : MonoBehaviour
     private float Damage;
     private bool IsDamaging;
 
-    private void OnTriggerEnter(Collider _collision)
+    private void OnCollisionEnter(Collision _collision)
     {
+        Debug.Log($"Hit Gameobject: {_collision.gameObject.name}");
+        if (!GameManager.instance)
+        {
+            
+            return;
+        }
         if (!IsDamaging || _collision.gameObject == GameManager.instance.playerGO)
             return;
         Attack(_collision.gameObject);
@@ -29,7 +36,10 @@ public class EnemyDamager : MonoBehaviour
 
     private void Attack(GameObject _playerGameObject)
     {
-        _playerGameObject.GetComponent<GotDamage>().GotHit(true);
+        _playerGameObject.TryGetComponent<GotDamage>(out GotDamage _gdmg);
+        if (!_gdmg)
+            throw new Exception("Player Got Damage not at Right place!");
+        _gdmg.GotHit(true);
         PlayerValueManager.instance.CurrHP -= Damage;
         PlayerValueManager.instance.healthSlider.value = PlayerValueManager.instance.CurrHP;
         IsDamaging = false;
