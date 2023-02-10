fcbb59c53e507444080c1d39e3610d9bd4b89438
Juri Weber <43060805+Jomshanseat@users.noreply.github.com>
2023-02-10 11:40:26 +0100

    Another batch of small and dirty fixes
    Changed naming and accessibility of some functions, while also putting in a dirty fix for the Player being able to glitch ontop of the skeletons due to it having a character controller instead of a self-written one with a y-locked rigidbody attached.

--- MedievalRPG/Assets/Scripts/Scripts/Enemy/Enemy_KI/Generic_Enemy/LookAter.cs
+++ MedievalRPG/Assets/Scripts/Scripts/Enemy/Enemy_KI/Generic_Enemy/LookAter.cs
@@ -7,17 +7,17 @@ using UnityEngine.Serialization;
 public class LookAter : MonoBehaviour
 {
     [SerializeField] private Animator Anim;
-    public Transform Head = null;
+    [SerializeField] private Transform Head;
 
-     public Vector3 LookAtTargetPos;
+    public Vector3 LookAtTargetPos;
 
-    [FormerlySerializedAs("LookAtCoolDown")] public float LookAtCoolTime = 0.2f;
-    public float LookAtHeatTime = 0.2f;
-    public bool IsLooking = true;
+    private const float LOOK_AT_COOL_TIME = 0.2f;
+    private const float LOOK_AT_HEAT_TIME = 0.2f;
+    [SerializeField]private bool IsLooking = true;
 
     private Vector3 LookAtPos;
     private float LookAtWeight = 0.0f;
-    
+
     void Start()
     {
         if (!Head)
@@ -28,20 +28,20 @@ public class LookAter : MonoBehaviour
 
         LookAtTargetPos = Head.position + transform.forward;
         LookAtPos = LookAtTargetPos;
-
     }
 
     private void OnAnimatorIK()
     {
         LookAtTargetPos.y = Head.position.y;
         float lookAtTargetWeight = IsLooking ? 1.0f : 0.0f;
-        Vector3 currentDir = LookAtPos - Head.position;
-        Vector3 futureDir = LookAtTargetPos - Head.position;
+        Vector3 headPos = Head.position;
+        Vector3 currentDir = LookAtPos - headPos;
+        Vector3 futureDir = LookAtTargetPos - headPos;
 
         currentDir = Vector3.RotateTowards(currentDir, futureDir, 6.28f * Time.deltaTime, float.PositiveInfinity);
-        LookAtPos = Head.position + currentDir;
+        LookAtPos = headPos + currentDir;
 
-        float blendTime = lookAtTargetWeight > LookAtWeight ? LookAtHeatTime : LookAtCoolTime;
+        float blendTime = lookAtTargetWeight > LookAtWeight ? LOOK_AT_HEAT_TIME : LOOK_AT_COOL_TIME;
         LookAtWeight = Mathf.MoveTowards(LookAtWeight, lookAtTargetWeight, Time.deltaTime / blendTime);
         Anim.SetLookAtWeight(LookAtWeight, 0.2f, 0.5f, 0.7f, 0.5f);
         Anim.SetLookAtPosition(LookAtPos);
