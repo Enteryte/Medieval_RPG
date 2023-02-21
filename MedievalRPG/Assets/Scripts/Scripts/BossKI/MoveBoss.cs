7c858275e9954518cd2c9f3ee9e8c06120acab68
Phill <phillip.hagemann@gmx.de>
2023-02-21 02:12:04 +0100

    Boss MovementFix

--- MedievalRPG/Assets/Scripts/Scripts/BossKI/MoveBoss.cs
+++ MedievalRPG/Assets/Scripts/Scripts/BossKI/MoveBoss.cs
@@ -43,35 +43,17 @@ public class MoveBoss : SkeletonBossActions
 
     private void PickNewTargetLocation()
     {
-        float minX = 0;
-        float maxX = 0;
-        float minZ = 0;
-        float maxZ = 0;
-
-        for (int x = 0; x < RoomEdges.Length; x++)
+        if (RoomEdges == null)
         {
-            if (x == 0)
-            {
-                maxX = RoomEdges[x].position.x;
-            }
-            if (x == 1)
-            {
-                minX = RoomEdges[x].position.x;
-            }
-
-            if (x == 2)
-            {
-                maxZ = RoomEdges[x].position.z;
-            }
-            if (x == 3)
-            {
-                minZ = RoomEdges[x].position.z;
-            }
+            RoomEdges = KI.RoomEdges;
         }
 
-        targetLocation = new Vector3(Random.Range(minX, maxX), 0, Random.Range(minZ, maxZ));
+        float minX = Mathf.Min(RoomEdges[0].position.x, RoomEdges[1].position.x, RoomEdges[2].position.x, RoomEdges[3].position.x);
+        float maxX = Mathf.Max(RoomEdges[0].position.x, RoomEdges[1].position.x, RoomEdges[2].position.x, RoomEdges[3].position.x);
+        float minZ = Mathf.Min(RoomEdges[0].position.z, RoomEdges[1].position.z, RoomEdges[2].position.z, RoomEdges[3].position.z);
+        float maxZ = Mathf.Max(RoomEdges[0].position.z, RoomEdges[1].position.z, RoomEdges[2].position.z, RoomEdges[3].position.z);
 
-        Debug.Log(targetLocation);
+        targetLocation = new Vector3(Random.Range(minX, maxX), 0, Random.Range(minZ, maxZ));
 
         agent.SetDestination(targetLocation);
         mayMove = true;
